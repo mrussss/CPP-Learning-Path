473c99802bc8a53a5c405a9f0dc57fffafd53b3f
mrussss <647139298@qq.com>
2026-03-17 16:37:30 +0800

    feat: complete data race and deadlock experiments, logger added

---
+++ Phase1.5_Bridge/GatewayBase/include/Logger.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <thread>
+#include <cstdio>
+#include <functional> // 必须引入这个来使用 std::hash
+
+// 使用哈希函数将 thread::id 转换为唯一的无符号整数 (size_t)
+#define LOG_INFO(fmt, ...)                                                                                         \
+    do                                                                                                             \
+    {                                                                                                              \
+        printf("[Thread %zu] " fmt "\n", std::hash<std::thread::id>{}(std::this_thread::get_id()), ##__VA_ARGS__); \
+    } while (0)
--- Phase1.5_Bridge/GatewayBase/src/main.cpp
+++ Phase1.5_Bridge/GatewayBase/src/main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include "Logger.hpp"
+#include <thread>
+#include <vector>
+#include <mutex>
 
+int shared_counter = 0;
+std::mutex mtx;
+void bad_worker()
+{
+    LOG_INFO("I begin to make trouble!");
+    for (int i = 0; i < 10000; ++i)
+    {
+        mtx.lock();
+        mtx.lock(); // 致命红线：对同一把普通的互斥锁连续锁两次！
+        shared_counter++;
+        mtx.unlock();
+    }
+}
 int main()
 {
-    // 放弃面面俱到，专注底层反馈。全靠报错和断点来教我！
-    std::cout << "Hello Gateway! Phase 1.5 Start." << std::endl;
+    std::vector<std::thread> threads;
+    for (int i = 0; i < 10; i++)
+    {
+        threads.emplace_back(bad_worker);
+    }
+    for (auto &t : threads)
+    {
+        t.join();
+    }
+    LOG_INFO("最终的 counter 结果是: %d (预期是 100000)", shared_counter);
     return 0;
 }
