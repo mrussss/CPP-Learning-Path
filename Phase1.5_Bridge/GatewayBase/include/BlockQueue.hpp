6295c03b621e722b219cb17c98617146c237e942
mrussss <647139298@qq.com>
2026-03-23 10:13:46 +0800

    feat: 实现 BlockQueue 具备明确语义的 pop 与优雅退出 stop，并配合谓词解决虚假唤醒

--- Phase1.5_Bridge/GatewayBase/include/BlockQueue.hpp
+++ Phase1.5_Bridge/GatewayBase/include/BlockQueue.hpp
@@ -1,6 +1,7 @@
 #include <queue>
 #include <mutex>
 #include <condition_variable>
+#include <utility>
 
 template <typename T>
 class BlockQueue
@@ -9,6 +10,7 @@ private:
     std::queue<T> data_queue;
     std::mutex mtx;
     std::condition_variable cv_;
+    bool _is_stopped = false;
 
 public:
     void push(const T &item)
@@ -17,5 +19,25 @@ public:
 
         data_queue.push(item);
         cv_.notify_one();
+    }
+    bool pop(T &item)
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv_.wait(lock, [this]
+                 { return !data_queue.empty() || _is_stopped; });
+        if (data_queue.empty() && _is_stopped)
+        {
+            return false;
+        }
+        item = std::move(data_queue.front());
+        data_queue.pop();
+        return true;
+    }
+
+    void stop()
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        _is_stopped = true;
+        cv_.notify_all();
     }
 };
---
+++ Phase1.5_Bridge/GatewayBase/src/main.cpp
@@ -0,0 +1,6 @@
+#include "BlockQueue.hpp"
+
+int main()
+{
+    return 0;
+}
