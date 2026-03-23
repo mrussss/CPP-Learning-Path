b36d680743de894726ac9bce14749662b3550866
mrussss <647139298@qq.com>
2026-03-23 14:31:26 +0800

    feat: 完成基于条件变量的线程安全阻塞队列，跑通优雅退出机制

--- Phase1.5_Bridge/GatewayBase/tests/test_blockqueue.cpp
+++ Phase1.5_Bridge/GatewayBase/tests/test_blockqueue.cpp
@@ -1,25 +1,39 @@
 #include <thread>
-#include <vector>
+#include <iostream>
+#include <chrono>
 #include "BlockQueue.hpp"
-int main()
-{
-    BlockQueue<int> queue;
-    std::vector<std::thread> threads;
 
+BlockQueue<int> queue;
+
+void Producer()
+{
     for (int i = 0; i < 10; i++)
     {
-        threads.emplace_back([&queue]()
-                             {
-                             for (int j = 0; j < 100000; j++)
-                             {
-                                queue.push(j);
-                             } });
+        queue.push(i);
+        std::cout << "Producer生产成功!" << std ::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+}
 
-    for (auto &t : threads)
+void Consumer()
+{
+    while (true)
     {
-        t.join();
+        int item;
+        bool state = queue.pop(item);
+        if (!state)
+        {
+            std::cout << "Consumer 收到退出信号，安全结束" << std ::endl;
+            break;
+        }
+        std::cout << item << std ::endl;
     }
-
-    return 0;
+}
+int main()
+{
+    std::thread T1(Producer);
+    std::thread T2(Consumer);
+    T1.join();
+    // queue.stop();
+    T2.join();
 }
